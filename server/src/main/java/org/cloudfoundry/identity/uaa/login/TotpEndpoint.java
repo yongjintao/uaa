cdf80d511ca20304d03376ece13a76d28370f8dd
Mikhail Vyshegorodtsev <medvedzver@inbox.ru>
2017-11-13 16:15:57 -0800

    Honor redirect url when registering MFA provider.
    [#152535588] https://www.pivotaltracker.com/story/show/152535588
    
    Signed-off-by: Bharath Sekar <bharath.sekar@ge.com>

--- server/src/main/java/org/cloudfoundry/identity/uaa/login/TotpEndpoint.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/login/TotpEndpoint.java
@@ -10,17 +10,24 @@ import org.cloudfoundry.identity.uaa.mfa.MfaProvider;
 import org.cloudfoundry.identity.uaa.mfa.MfaProviderProvisioning;
 import org.cloudfoundry.identity.uaa.mfa.UserGoogleMfaCredentialsProvisioning;
 import org.cloudfoundry.identity.uaa.zone.IdentityZoneHolder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -33,6 +40,8 @@ public class TotpEndpoint {
     private Log logger = LogFactory.getLog(TotpEndpoint.class);
 
     private GoogleAuthenticatorAdapter googleAuthenticatorService;
+
+    private SavedRequestAwareAuthenticationSuccessHandler redirectingHandler;
 
     @RequestMapping(value = {"/login/mfa/register"}, method = RequestMethod.GET)
     public String generateQrUrl(HttpSession session, Model model) throws NoSuchAlgorithmException, IOException {
@@ -63,10 +72,10 @@ public class TotpEndpoint {
         return "enter_code";
     }
 
-
     @RequestMapping(value = {"/login/mfa/verify.do"}, method = RequestMethod.POST)
-    public String validateCode(Model model,
+    public ModelAndView validateCode(Model model,
                                HttpSession session,
+                               HttpServletRequest request, HttpServletResponse response,
                                @RequestParam("code") String code)
             throws NoSuchAlgorithmException, IOException {
         UaaAuthentication sessionAuth = session.getAttribute(MFA_VALIDATE_USER) instanceof UaaAuthentication ? (UaaAuthentication) session.getAttribute(MFA_VALIDATE_USER) : null;
@@ -74,7 +83,7 @@ public class TotpEndpoint {
         if(sessionAuth != null) {
             uaaPrincipal = sessionAuth.getPrincipal();
         } else {
-            return "redirect:/login";
+            return new ModelAndView("redirect:/login", Collections.emptyMap());
         }
 
         try {
@@ -86,19 +95,23 @@ public class TotpEndpoint {
                 authMethods.addAll(Arrays.asList("otp", "mfa"));
                 sessionAuth.setAuthenticationMethods(authMethods);
                 SecurityContextHolder.getContext().setAuthentication(sessionAuth);
-                return "redirect:/";
+                redirectingHandler.onAuthenticationSuccess(request, response, sessionAuth);
+                return new ModelAndView("home", Collections.emptyMap());
             }
             logger.debug("Code authorization failed for user: " + uaaPrincipal.getId());
-            model.addAttribute("error", "Invalid QR code");
+            model.addAttribute("error", "Incorrect code, please try again.");
         } catch (NumberFormatException e) {
             logger.debug("Error validating the code for user: " + uaaPrincipal.getId() + ". Error: " + e.getMessage());
-            model.addAttribute("error", "QR code can be number only");
+            model.addAttribute("error", "Code can be numbers only");
         } catch (GoogleAuthenticatorException e) {
             logger.debug("Error validating the code for user: " + uaaPrincipal.getId() + ". Error: " + e.getMessage());
-            model.addAttribute("error", "Invalid QR code");
+            model.addAttribute("error", "Incorrect code, please try again.");
+        } catch (ServletException e) {
+            logger.debug("Error redirecting user: " + uaaPrincipal.getId() + ". Error: " + e.getMessage());
+            model.addAttribute("error", "Can't redirect user");
         }
         model.addAttribute("is_first_time_user", userGoogleMfaCredentialsProvisioning.isFirstTimeMFAUser(uaaPrincipal));
-        return "enter_code";
+        return new ModelAndView("enter_code", model.asMap());
     }
 
     public void setUserGoogleMfaCredentialsProvisioning(UserGoogleMfaCredentialsProvisioning userGoogleMfaCredentialsProvisioning) {
@@ -120,5 +133,9 @@ public class TotpEndpoint {
         } else {
             return null;
         }
+    }
+
+    public void setRedirectingHandler(SavedRequestAwareAuthenticationSuccessHandler handler) {
+        this.redirectingHandler = handler;
     }
 }
