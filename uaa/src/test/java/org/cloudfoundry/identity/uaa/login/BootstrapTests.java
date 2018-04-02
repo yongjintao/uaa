b04038b4d5f910e3f18733cf8ed2036243dfbf22
Jennifer Hamon <jhamon@pivotal.io>
2018-04-02 16:56:52 -0700

    Correctly labeled ITs, removed OMS refs, and started using URL builder.
    Part of saga to ensure unit tests have no external dependencies.
    
    [#154653975] https://www.pivotaltracker.com/story/show/154653975
    
    Signed-off-by: Jaskanwal Pawar <jpawar@pivotal.io>

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/login/BootstrapTests.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/login/BootstrapTests.java
@@ -942,7 +942,7 @@ public class BootstrapTests {
     @Test
     public void legacy_saml_idp_as_top_level_element() throws Exception {
         System.setProperty("login.saml.metadataTrustCheck", "false");
-        System.setProperty("login.idpMetadataURL", "http://simplesamlphp.oms.identity.team/saml2/idp/metadata.php");
+        System.setProperty("login.idpMetadataURL", "http://simplesamlphp.uaa.com/saml2/idp/metadata.php");
         System.setProperty("login.idpEntityAlias", "testIDPFile");
 
         context = getServletContext("default", "login.yml","uaa.yml", "file:./src/main/webapp/WEB-INF/spring-servlet.xml");
@@ -985,7 +985,7 @@ public class BootstrapTests {
     @Test
     public void legacy_saml_metadata_as_url() throws Exception {
         System.setProperty("login.saml.metadataTrustCheck", "false");
-        System.setProperty("login.idpMetadataURL", "http://simplesamlphp.oms.identity.team:80/saml2/idp/metadata.php");
+        System.setProperty("login.idpMetadataURL", "http://simplesamlphp.uaa.com:80/saml2/idp/metadata.php");
         System.setProperty("login.idpEntityAlias", "testIDPUrl");
 
         context = getServletContext("default", "login.yml","uaa.yml", "file:./src/main/webapp/WEB-INF/spring-servlet.xml");
@@ -1006,7 +1006,7 @@ public class BootstrapTests {
     @Test
     public void legacy_saml_url_without_port() throws Exception {
         System.setProperty("login.saml.metadataTrustCheck", "false");
-        System.setProperty("login.idpMetadataURL", "http://simplesamlphp.oms.identity.team/saml2/idp/metadata.php");
+        System.setProperty("login.idpMetadataURL", "http://simplesamlphp.uaa.com/saml2/idp/metadata.php");
         System.setProperty("login.idpEntityAlias", "testIDPUrl");
 
         context = getServletContext("default", "login.yml","uaa.yml", "file:./src/main/webapp/WEB-INF/spring-servlet.xml");
